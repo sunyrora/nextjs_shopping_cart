4c8e2c970ee96f8a211853830d03ed05c27b4a0b
sunyrora <sunyrora@gmail.com>
2022-06-28 10:40:08 +0200

    refactor: graphql cors usin micro-cors

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,44 +1,50 @@
 import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "../../graphql/type-def";
 import { resolvers } from "../../graphql/resolvers";
-// import Cors from "micro-cors";
+import Cors from "micro-cors";
 
-// const cors = Cors();
+const cors = Cors({
+  allowMethods: ["POST", "OPTIONS"],
+});
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 const startServer = apolloServer.start();
 
-// export default cors(async function handler(req, res) {
-//   await startServer;
-
-//   await apolloServer.createHandler({
-//     path: "/api/graphql",
-//   })(req, res);
-// });
-
-export default async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://studio.apollographql.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
+export default cors(async (req, res) => {
   if (req.method === "OPTIONS") {
     res.end();
     return false;
   }
-
   await startServer;
 
   await apolloServer.createHandler({
     path: "/api/graphql",
   })(req, res);
-};
+});
+
+// export default async (req, res) => {
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://studio.apollographql.com"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+
+//   if (req.method === "OPTIONS") {
+//     res.end();
+//     return false;
+//   }
+
+//   await startServer;
+
+//   await apolloServer.createHandler({
+//     path: "/api/graphql",
+//   })(req, res);
+// };
 
 export const config = {
   api: {
