ec9bd60d010c9ac0133cfadecef4704f35d2ff48
sunyrora <sunyrora@gmail.com>
2022-07-01 19:21:20 +0200

    GraphQL URI from .env

--- apolloClient/apolloClient.js
+++ apolloClient/apolloClient.js
@@ -1,13 +1,10 @@
-import { InMemoryCache, ApolloClient, HttpLink } from "@apollo/client";
+import { ApolloClient } from "@apollo/client";
 import { cache } from "./cache";
 import { typeDefs } from "./type-def";
 
-// cache
-// const cache = new InMemoryCache();
-
 const apolloClient = new ApolloClient({
-  // uri: "http://localhost:3000/api/graphql",
-  uri: `http://localhost:${process.env.PORT}/api/graphql`,
+  uri: process.env.GRAPH_URI,
+  // uri: `http://localhost:${process.env.PORT}/api/graphql`,
   cache: cache,
   typeDefs,
   connectToDevTools: process.env.NODE_ENV !== "production",
